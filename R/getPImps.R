78584a2107f85924fd8510545812e5985d60e727
Holger Schwender <holger.schw@gmx.de>
2012-01-02 00:00:00 +0000

    version 1.1.1

--- R/getPImps.R
+++ R/getPImps.R
@@ -4,7 +4,7 @@ getPImps<-function(ltree,type){
 	if(type==1 && all(ltree$trees$pick==0))
 		return(NULL)
 	mat.truth<-generateTruthTab(ltree)
-	truth<-ifelse(ltree$coef>0 | is.na(ltree$coef),1,0)
+	truth<-ifelse(ltree$coef>=0 | is.na(ltree$coef),1,0)
 	ids.truth<-mat.truth[,"outcome"]==truth
 	mat.truth<-mat.truth[ids.truth,-ncol(mat.truth),drop=FALSE]
 	if(sum(ids.truth)==1)
